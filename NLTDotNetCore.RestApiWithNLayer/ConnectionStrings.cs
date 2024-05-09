ffc5de263a700376f2bab72149c071814efaa2d5
Nay Lin Tun <naylin.ucsy.99@gmail.com>
2024-05-09 23:05:59 +0630

    [Complete] RestApiWithNLayer CRUD

---
+++ NLTDotNetCore.RestApiWithNLayer/ConnectionStrings.cs
@@ -0,0 +1,15 @@
+using System.Data.SqlClient;
+
+namespace NLTDotNetCore.RestApiWithNLayer;
+
+internal static class ConnectionStrings
+{
+    public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+    {
+        DataSource = ".", // server name
+        InitialCatalog = "DotNetTrainingBatch4", // database name
+        UserID = "sa", // username
+        Password = "sasa@123", // password
+        TrustServerCertificate = true,
+    };
+}
