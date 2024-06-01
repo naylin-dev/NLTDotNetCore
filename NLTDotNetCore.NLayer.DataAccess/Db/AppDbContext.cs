749fd5513e6771121a188f7ba82df676313ccdc0
Nay Lin Tun <naylin.ucsy.99@gmail.com>
2024-06-01 21:39:24 +0630

    [Complete] NLayer Architecture

---
+++ NLTDotNetCore.NLayer.DataAccess/Db/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using NLTDotNetCore.NLayer.DataAccess.Models;
+
+namespace NLTDotNetCore.NLayer.DataAccess.Db;
+
+internal class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+    }
+
+    public DbSet<BlogModel> Blogs { get; set; }
+}
---
+++ NLTDotNetCore.NLayer.UI/Program.cs
@@ -0,0 +1,15 @@
+﻿using NLTDotNetCore.NLayer.BusinessLogic.Services;
+
+BL_Blog blBlog = new BL_Blog();
+
+var item = blBlog.GetBlog(1);
+if (item is null)
+{
+    Console.WriteLine("No Data Found.");
+    return;
+}
+
+Console.WriteLine("Id: " + item.BlogId);
+Console.WriteLine("Title: " + item.BlogTitle);
+Console.WriteLine("Author: " + item.BlogAuthor);
+Console.WriteLine("Content: " + item.BlogContent);
