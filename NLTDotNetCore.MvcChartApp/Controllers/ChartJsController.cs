32be3a13433cc06c3c5b24be21277f5d242082c5
Nay Lin Tun <naylin.ucsy.99@gmail.com>
2024-07-11 23:30:19 +0630

    [Complete] FloatingBarsChart from ChartJs

---
+++ NLTDotNetCore.MvcChartApp/Controllers/ChartJsController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using NLTDotNetCore.MvcChartApp.Models;
+
+namespace NLTDotNetCore.MvcChartApp.Controllers;
+
+public class ChartJsController : Controller
+{
+    /// <summary>
+    /// Generates an array of two random numbers within the specified range.
+    /// </summary>
+    /// <param name="max">The maximum value of the range (inclusive).</param>
+    /// <param name="min">The minimum value of the range (inclusive).</param>
+    /// <returns>An array of two random numbers within the specified range.</returns>
+    public int[] GenerateRandomNumber(int max, int min)
+    {
+        Random rand = new Random();
+        return new[] { rand.Next(min, max + 1), rand.Next(min, max + 1) };
+    }
+
+    // GET
+    public IActionResult FloatingBarsChart()
+    {
+        FloatingBarsChartModel model = new FloatingBarsChartModel()
+        {
+            Labels = new[]
+            {
+                "January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+                "November", "December"
+            },
+            Data1 = new int[12][],
+            Data2 = new int[12][],
+        };
+
+        for (int i = 0; i < 12; i++)
+        {
+            model.Data1[i] = GenerateRandomNumber(100, -100);
+            model.Data2[i] = GenerateRandomNumber(100, -100);
+        }
+
+        return View(model);
+    }
+}
---
+++ NLTDotNetCore.MvcChartApp/Models/FloatingBarsChartModel.cs
@@ -0,0 +1,8 @@
+namespace NLTDotNetCore.MvcChartApp.Models;
+
+public class FloatingBarsChartModel
+{
+    public string[] Labels { get; set; }
+    public int[][] Data1 { get; set; }
+    public int[][] Data2 { get; set; }
+}
